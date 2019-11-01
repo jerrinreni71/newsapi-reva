4da6562787d7940b185e1b9dca7b66605dcbb5b4
Akhil VC <akhilvc10@gmail.com>
2019-11-02 01:14:22 +0530

    implement post

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+function App () {
+  const [category, setCategory] = React.useState( "" );
+  const [data, setData] = React.useState( [] );
+  const [loading, setLoading] = React.useState( false );
 
-function App() {
+  React.useEffect( () => {
+    console.log( category )
+  }, [category] )
+
+
+  const postData = () => {
+
+    setLoading( true )
+    let data = {
+      category
+    }
+    axios
+      .post(
+        "https://gwfzy6xxo3.execute-api.us-west-1.amazonaws.com/dev/latest",
+        data
+      )
+      .then( response => {
+        let newsData = JSON.parse( response.data )
+        setData( newsData.headlines )
+        setLoading( false )
+
+        console.log( response )
+        // this.setState( { loading: false, completed: true } );
+      } )
+      .catch( e => console.log( e ) );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        id="food_name"
+        type="text"
+        className="validate"
+        value={category}
+        placeholder="Type Your Keywords"
+        onChange={e => setCategory( e.target.value )}
+      />
+      <button onClick={postData}>
+        Search By Keyword
+      </button>
+      <ul>
+
+        {
+          data.length > 0 ? data.map( d => <li>{d} </li> ) : <li>{!loading ? "Search to see the result" : "Loading..."} </li>
+        }
+      </ul>
     </div>
   );
 }
+
+
 
 export default App;
